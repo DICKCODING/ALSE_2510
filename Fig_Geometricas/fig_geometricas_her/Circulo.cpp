8a8cf1581ad030f94ff65284c4aeb10dad1d58bc
DICKCODING <dickson.trujillo-t@mail.escuelaing.edu.co>
2025-03-16 17:16:11 -0500

    INTERSECCIONCIRCULOS:DICKSON TRUJILLO, JUAN CORTES

--- Fig_Geometricas/fig_geometricas_her/Circulo.cpp
+++ Fig_Geometricas/fig_geometricas_her/Circulo.cpp
@@ -1,34 +1,43 @@
 /**
  * Project Untitled
  */
-
-
 #include "Circulo.h"
 #include <math.h>
+#include <cmath>
 /**
  * Circulo implementation
  */
-
-
 /**
  * @param r
  * @param x
  * @param y
  */
-Circulo::Circulo(float r, float x, float y) {
+Circulo::Circulo(float r, float x, float y)
+{
     _radio = r;
-    setX( x );
-    setY( y );
+    setX(x);
+    setY(y);
 }
 
-Circulo::Circulo() {
-
+Circulo::Circulo()
+{
 }
 
-float Circulo::area(){
+float Circulo::area()
+{
     return M_PI * _radio * _radio;
 }
 
-float Circulo::perimetro(){
+float Circulo::perimetro()
+{
     return 2 * M_PI * _radio;
+}
+bool Circulo::intersectaCon(const Circulo &otro)
+{
+    float distancia = sqrt(pow(getX() - otro.getX(), 2) + pow(getY() - otro.getY(), 2));
+    if (distancia <= (_radio + otro._radio))
+    {
+        return true;
+    }
+    return false;
 }
--- Fig_Geometricas/fig_geometricas_her/Circulo.h
+++ Fig_Geometricas/fig_geometricas_her/Circulo.h
@@ -1,32 +1,34 @@
 /**
  * Project Untitled
  */
-
+// By: Juan Esteban Cortés Y Dickson Trujillo
 
 #ifndef _CIRCULO_H
 #define _CIRCULO_H
 
 #include "Geometrica.h"
+#include <cmath>
 
+class Circulo : public Geometrica
+{
+public:
+	/**
+	 * @param r
+	 * @param x
+	 * @param y
+	 */
+	Circulo(float r, float x = 0., float y = 0.);
+	float area();
+	float perimetro();
+	bool intersectaCon(const Circulo &otro);
 
-class Circulo: public Geometrica {
-public: 
-	
-/**
- * @param r
- * @param x
- * @param y
- */
-Circulo(float r, float x = 0., float y = 0.);
-float area();
-float perimetro();
+	void setRadio(float r) { _radio = r; }
+	float getRadio() const { return _radio; }
 
-void setRadio(float r){_radio = r;}
-float getRadio(){return _radio;}
-private: 
+private:
 	float _radio;
-	
-Circulo();
+
+	Circulo();
 };
 
 #endif //_CIRCULO_H
