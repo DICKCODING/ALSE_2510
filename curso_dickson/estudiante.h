55e81271f532c535987b5e5b8495c60e59843110
DICKCODING <dickson.trujillo-t@mail.escuelaing.edu.co>
2025-03-18 22:06:19 -0500

    ADELANTO TRABAJO PROMEDIO ESTUDIANTES V1.0 DICKSON

--- curso_dickson/estudiante.h
+++ curso_dickson/estudiante.h
@@ -1,6 +1,7 @@
 
 #include "persona.h"
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -8,13 +9,13 @@ class Estudiante : public Persona
 {
     vector<double> notas;
     int codigo;
+    void Codigo(const int &cod);
+    double Nota(const int &i);
 
 public:
     Estudiante(const int &cod);
     Estudiante(const string &nombre, const string &apellido, const int &docid, const int &cod);
     int Codigo();
-    void Codigo(const int &cod);
-    double Nota(const int &i);
     void agregarNota(const double &nota);
     double promedio();
     friend ostream &operator<<(ostream &os, const Estudiante &e);
--- curso_dickson/persona.h
+++ curso_dickson/persona.h
@@ -6,13 +6,20 @@ using namespace std;
 
 class Persona
 {
+public:
   string nombre;
   string apellido;
   int doc_identidad;
 
-public:
+  Persona();
   Persona(int docid);
 
   string Nombre();
   void Nombre(const string &nombre);
+  string Apellido();
+  void Apellido(const string &apellido);
+  int Doc_identidad();
+  void Doc_identidad(const int &docid);
+
+  ~Persona();
 };
